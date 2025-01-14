36dba28d8e973a2beb52d27a13a19587182e8a80
Sijan Paudel <sijanpaudel44@gmail.com>
2025-01-15 02:44:25 +0545

    created assets.ts file for all images

---
+++ src/assets/assets.ts
@@ -0,0 +1,45 @@
+import Facebook from './facebook.png';
+import Instagram from './instagram.png';
+import Youtube from './youtube.png';
+import Pinterest from './pinterest.png';
+import Mastercard from './mastercard.png';
+import Paypal from './paypal.png';
+import Visa from './visa.png';
+import Skrill from './skrill.png';
+import Woman from './woman.png';
+import Star from './star.png';
+import Product from './product.png';
+import Discover from './discover.png';
+import Menu from './menu.png';
+import Cross from './cross.png';
+import Cart from './cart.png';
+import Notification from './notification.png';
+import Search from './search.png';
+import X from './x.png';
+import Category from './category.png';
+import Logo from './logo.png';
+import Profile from './profile.png';
+
+export {
+  Facebook,
+  Instagram,
+  Youtube,
+  Pinterest,
+  Mastercard,
+  Paypal,
+  Visa,
+  Skrill,
+  Woman,
+  Star,
+  Product,
+  Discover,
+  Menu,
+  Cross,
+  Cart,
+  Notification,
+  Search,
+  X,
+  Category,
+  Logo,
+  Profile,
+};
